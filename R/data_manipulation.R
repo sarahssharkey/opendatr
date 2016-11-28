d1e8de7368f20d0eeb66299429b9caff5ade1d2c
Joe O'Hara <joe.is.ohara@gmail.com>
2016-11-28 18:23:31 +0000

    Changing getCommonDataVariables to getIntersectOfMany

--- R/data_manipulation.R
+++ R/data_manipulation.R
@@ -3,25 +3,14 @@
 #' @name opendatr
 NULL
 
-#' A function for getting common data variables from N data sets
+#' A function for getting common intersection of N lists
 #'
-#' This function takes N data sets and returns their common data variables
-#' @param ... Datasets passed as parameters
+#' This function takes N lists and returns their common element(s)
+#' @param ... Lists passed as parameters
 #' @keywords common,intersect
 #' @export
 #' @examples
-#' getCommonDataVariables()
-getCommonDataVariables <- function(...){
-  args <- list(...)
-  lengthOfArgs <- length(args)
-  if(lengthOfArgs < 2) stop("Not enough data sets provided")
-  intersectionOfSets <- c()
-  a <- args[[1]]
-  b <- args[[2]]
-  intersectionOfSets <- intersect(a,b)
-  for(arg in args[3:lengthOfArgs]){
-    if(!is.null(arg))
-      intersectionOfSets <- intersect(intersectionOfSets,arg)
-  }
-  return(intersectionOfSets)
+#' getIntersectOfMany()
+getIntersectOfMany <- function(...){
+  return(Reduce(intersect, list(...)))
 }

e02178cd77c0b0f119b79ed86664baa9c2918d5e
Joe O'Hara <joe.is.ohara@gmail.com>
2016-11-29 15:13:18 +0000

    Function now returns the common data variables for data sets

--- R/data_manipulation.R
+++ R/data_manipulation.R
@@ -3,14 +3,20 @@
 #' @name opendatr
 NULL
 
-#' A function for getting common intersection of N lists
+#' A function for getting common data variables of N data sets
 #'
-#' This function takes N lists and returns their common element(s)
+#' This function takes N data sets and returns their common data variable(s)
 #' @param ... Lists passed as parameters
-#' @keywords common,intersect
+#' @keywords common,intersect,data,variable
 #' @export
-#' @examples
-#' getIntersectOfMany()
-getIntersectOfMany <- function(...){
-  return(Reduce(intersect, list(...)))
+#' getCommonDataVariables
+getCommonDataVariables <- function(...) {
+  intersectNames <- function(x,y) {
+    if(is.null(colnames(x))){
+      return(intersect(x,colnames(y)))
+    } else {
+      return(intersect(colnames(x),colnames(y)))
+    }
+  }
+  return(Reduce(intersectNames, list(...)))
 }

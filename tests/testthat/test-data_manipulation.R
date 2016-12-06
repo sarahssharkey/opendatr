642c207584ce587588f434a3f2070a8f4c2f334c
Geoffrey Natin <nating@tcd.ie>
2016-12-06 16:56:26 +0000

    Adding getVariables() function to package.

--- tests/testthat/test-data_manipulation.R
+++ tests/testthat/test-data_manipulation.R
@@ -3,29 +3,60 @@ library(opendatr)
 context("Data manipulation")
 
 test_that("getCommonDataVariables runs as expected for 1D", {
-  test_1 <- c("x","y","z")
-  test_2 <- c("x","y")
-  names(test_1) <- c("a","b","c")
-  names(test_2) <- c("b","c")
-  common <- c("b","c")
-  result <- getCommonDataVariables(test_1, test_2)
-  expect_equivalent(result, common)
+    test_1 <- c("x","y","z")
+    test_2 <- c("x","y")
+    names(test_1) <- c("a","b","c")
+    names(test_2) <- c("b","c")
+    common <- c("b","c")
+    result <- getCommonDataVariables(test_1, test_2)
+    expect_equivalent(result, common)
 })
 
 test_that("getCommonDataVariables runs as expected for 2D", {
-  result <- getCommonDataVariables(women,PlantGrowth)
-  expect_equivalent(result,c("weight"))
+    result <- getCommonDataVariables(women,PlantGrowth)
+    expect_equivalent(result,c("weight"))
 })
 
 test_that("getCommonDataVariables runs as expected for 1D odd no. datasets", {
-  test_1 <- c("x","y","z")
-  test_2 <- c("x","y")
-  test_3 <- c("blah","blahs","wow")
-  names(test_1) <- c("a","b","c")
-  names(test_2) <- c("b","c")
-  names(test_3) <- c("c","d","e")
+    test_1 <- c("x","y","z")
+    test_2 <- c("x","y")
+    test_3 <- c("blah","blahs","wow")
+    names(test_1) <- c("a","b","c")
+    names(test_2) <- c("b","c")
+    names(test_3) <- c("c","d","e")
+    
+    common <- c("c")
+    result <- getCommonDataVariables(test_1, test_2, test_3)
+    expect_equivalent(result, common)
+})
 
-  common <- c("c")
-  result <- getCommonDataVariables(test_1, test_2, test_3)
-  expect_equivalent(result, common)
+
+test_that("getVariables runs as expected for 1D", {
+    test_1 <- c("x","y","z")
+    test_2 <- c("x","y")
+    names(test_1) <- c("a","b","g")
+    names(test_2) <- c("b","c")
+    vars <- c("a","b","g","c")
+    cat(vars)
+    result <- getVariables(test_1, test_2)
+    cat(result)
+    expect_equivalent(result, vars)
+})
+
+test_that("getVariables runs as expected for 2D", {
+    result <- getVariables(women,PlantGrowth)
+    expect_equivalent(result,c("height","weight","group"))
+})
+
+test_that("getVariables runs as expected for 1D odd no. datasets", {
+    test_1 <- c("x","y","z")
+    test_2 <- c("x","y")
+    test_3 <- c("blah","blahs","wow")
+    names(test_1) <- c("a","b","c")
+    names(test_2) <- c("b","c")
+    names(test_3) <- c("c","d","e")
+    
+    vars <- c("a","b","c","d","e")
+    result <- getVariables(test_1, test_2, test_3)
+    expect_equivalent(result, vars)
 })

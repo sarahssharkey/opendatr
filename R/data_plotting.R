4541740e77ef6c366d639d6e7f7d2d9f809c79d2
Mark Hanley <mkhanley1@gmail.com>
2016-12-09 12:54:53 +0000

    Plot function added

--- R/data_plotting.R
+++ R/data_plotting.R
@@ -9,10 +9,11 @@ NULL
 #' @param y y value to be plotted
 #' @param xlabel label for x axis
 #' @param ylabel label for y axis
+#' @param plotType Type of plot wanted (l = line, h = bar etc)
 #' @keywords data,plot
 #' @export
 #' plotDataSet
-plotDataSet <- function(x, y, xlabel, ylabel) {
-  plot(x,y, xlab = xlabel, ylab = ylabel, type = "b", main = sprintf("%s over %s", ylabel, xlabel))
-
+plotDataSet <- function(x, y, xlabel, ylabel, plotType) {
+  plot(x,y, xlab = xlabel, ylab = ylabel, type = plotType, main = sprintf("%s over %s", ylabel, xlabel))
+  #axis(side = 1, cex = 0.5)#, at = x, cex = .1)
 }

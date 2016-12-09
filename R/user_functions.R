7d50fa1caf99520047f2062a9b7af8b15fb59a8c
Sarah Sharkey <sasharke@tcd.ie>
2016-12-09 14:01:45 +0000

    Add user function for plotting, unfinished without plotting yet.

--- R/user_functions.R
+++ R/user_functions.R
@@ -28,3 +28,60 @@ getDatasetInfo <- function(...) {
 
   return(returnVal)
 }
+
+#' A function that plots N datasets on graph(s) based on the information entered by the user
+#'
+#' This function takes a list of information in the specified format
+#' list parameter =
+#'[ urlList = [dataset0 = url0, dataset1 = url1, ...., datasetn = urln],
+#'  graphType = [graph1, graph2, ..... graphn],
+#'  commonVariable = var || [commonVar0, commonVar1, .... , commonVarn],
+#'  yAxis = [dataset0 = colName, dataset1 = colName, ...., datasetN = colName],
+#'  factors = [
+#'    dataset0factors = [factor0 = val0, factor = val1, factor2 = val2, …., factorn = valn],
+#'    dataset1factors = [factor0 = val0, factor1 = val1, factor2 = val2, …., factorn = valn],
+#'    ....
+#'    datasetNfactors = [factor0 = val0, factor1 = val1, factor2 = val2, …., factorn = valn]
+#'    ]]
+#' @param ... List in above format specifying the plot the user would like
+#' @keywords common,intersect,data,variable
+#' @export
+#' plotDatasets
+plotDatasets <- function(plotInfo) {
+  urls <- plotInfo[["urlList"]]
+  datasets <- apply((matrix(as.vector(unlist(urls)), nrow = 1, ncol = length(urls))), 2, getDataSet)
+  graphType <- plotInfo[["graphType"]]
+  commonVariable <- plotInfo[["commonVariable"]]
+  if (class(commonVariable) == "character"){
+    classType <- class(datasets[[1]][[commonVariable]])
+    for (i in 2:length(datasets)){
+      newClassType <- class(datasets[[i]][[commonVariable]])
+      if (newClassType != classType){
+        print(paste("incompatible common variable types", classType, newClassType))
+      }
+    }
+  }
+  else if (class(commonVariable) == "list"){
+    commonVar <- commonVariable[[1]]
+    classType <- class(datasets[[1]][[commonVar]])
+    for (i in 2:length(commonVariable)){
+      commonVar <- commonVariable[[i]]
+      newClassType <- class(datasets[[i]][[commonVar]])
+      if (newClassType != classType){
+        print(paste("incompatible common variable types", classType, newClassType))
+      }
+    }
+  }
+  factors <- plotInfo[["factors"]]
+  for (i in 1:length(factors)){
+    subDataset <- datasets[[i]]
+    datasetFactors <- factors[[i]]
+    factorNames <- names(datasetFactors)
+    for (j in 1:length(datasetFactors)){
+      subDataset <- subset(subDataset, subDataset[[factorNames[j]]] == datasetFactors[[j]])
+    }
+    datasets[[i]] <- subDataset
+  }
+
+
+}

e9d95a696ff2b5500369915ac918614cb7853e86
Mark Hanley <mkhanley1@gmail.com>
2016-12-09 11:55:38 +0000

    Primative plotting

--- R/data_plotting.R
+++ R/data_plotting.R
@@ -2,3 +2,17 @@
 #' @docType package
 #' @name opendatr
 NULL
+
+#' A function that plots data based on the x and y values passed in
+#'
+#' @param x x value to be plotted
+#' @param y y value to be plotted
+#' @param xlabel label for x axis
+#' @param ylabel label for y axis
+#' @keywords data,plot
+#' @export
+#' plotDataSet
+plotDataSet <- function(x, y, xlabel, ylabel) {
+  plot(x,y, xlab = xlabel, ylab = ylabel, type = "b", main = sprintf("%s over %s", ylabel, xlabel))
+
+}

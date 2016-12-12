a91e920b3e04b276a804794a50ec63ddc4a09313
Geoffrey Natin <nating@tcd.ie>
2016-12-12 18:07:16 +0000

    Updating getDataSet to getDataset.

--- tests/testthat/test-data_manipulation.R
+++ tests/testthat/test-data_manipulation.R
@@ -31,32 +31,3 @@ test_that("getCommonDataVariables runs as expected for 1D odd no. datasets", {
     result <- getCommonDataVariables(test_list)
     expect_equivalent(result, common)
 })
-
-
-test_that("getVariables runs as expected for 1D", {
-    test_1 <- c("x","y","z")
-    test_2 <- c("x","y")
-    names(test_1) <- c("a","b","g")
-    names(test_2) <- c("b","c")
-    vars <- c("a","b","g","c")
-    result <- getVariables(test_1, test_2)
-    expect_equivalent(result, vars)
-})
-
-test_that("getVariables runs as expected for 2D", {
-    result <- getVariables(women,PlantGrowth)
-    expect_equivalent(result,c("height","weight","group"))
-})
-
-test_that("getVariables runs as expected for 1D odd no. datasets", {
-    test_1 <- c("x","y","z")
-    test_2 <- c("x","y")
-    test_3 <- c("blah","blahs","wow")
-    names(test_1) <- c("a","b","c")
-    names(test_2) <- c("b","c")
-    names(test_3) <- c("c","d","e")
-    
-    vars <- c("a","b","c","d","e")
-    result <- getVariables(test_1, test_2, test_3)
-    expect_equivalent(result, vars)
-})

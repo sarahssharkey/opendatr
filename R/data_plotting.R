e697e7eb0ef9b6d5a2aaed1c3cea20785f0ee0ff
Sarah Sharkey <sasharke@tcd.ie>
2016-12-12 12:12:59 +0000

    Add several graphs plot at once.

--- R/data_plotting.R
+++ R/data_plotting.R
@@ -45,38 +45,40 @@ plotDataSet <- function(x, y, xlabel, ylabel, plotTypeStr) {
 #' @keywords data,plot
 #' @export
 #' plotDataSet
-plotDataSet <- function(dataset, plots) {
-  for(plot in plots){
-    frame()
-    for(set in dataset){
-      x = set[[1]]
-      y = set[[2]]
-      xlabel = set[[3]]
-      ylabel = set[[4]]
-      if(plot != "pie"){
-        plotType = ""
-        switch(plot,
-               "line" = {
-                 plotType = "l"
-               },
-               "bar" = {
-                 plotType = "h"
-               },
-               "scatter" = {
-                 plotType = "p"
-               }
-        )
-        plot(x,y, xlab = xlabel, ylab = ylabel, type = plotType, main = sprintf("%s over %s", ylabel, xlabel))
-        #lines(x = c(2003:2017), y = c(10000,100000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,1000), col = "red")
+plotDataSet <- function(plotInfo) {
+  graphType <- plotInfo[["graphTypes"]]
+  datasets <- plotInfo[["datasets"]]
 
-        #axis(side = 1, cex = 0.5)#, at = x, cex = .1)
-      }
-      else if(plot == "pie"){
-        percentages <- round(y/sum(y)*100)
-        percentLabel = paste(x, sprintf(" - %s%%", percentages))
-        pie(y, main = sprintf("%s per %s", ylabel, xlabel), labels = percentLabel, col = rainbow(length(x)))
-      }
+  par(mfrow=c(length(datasets),1))
+
+  for(set in datasets){
+    x = set[["xVals"]]
+    y = set[["yVals"]]
+    xlabel = set[["xLabel"]]
+    ylabel = set[["yLabel"]]
+    if(graphType != "pie"){
+      plotType = ""
+      switch(graphType,
+             "line" = {
+               plotType = "l"
+             },
+             "bar" = {
+               plotType = "h"
+             },
+             "scatter" = {
+               plotType = "p"
+             }
+      )
+      plot(x,y, xlab = xlabel, ylab = ylabel, type = plotType, main = sprintf("%s over %s", ylabel, xlabel), cex = 1/length(x))
+
+      #lines(x = c(2003:2017), y = c(10000,100000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,10000,1000), col = "red")
+
+      #axis(side = 1, cex = 0.5)#, at = x, cex = .1)
+    }
+    else if(plot == "pie"){
+      percentages <- round(y/sum(y)*100)
+      percentLabel = paste(x, sprintf(" - %s%%", percentages))
+      pie(y, main = sprintf("%s per %s", ylabel, xlabel), labels = percentLabel, col = rainbow(length(x)))
     }
   }
 }
-
